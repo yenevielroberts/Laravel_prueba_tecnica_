cbfeb4188651c1eaa11fc54a9ed262c1d0a5c49d
Yeneviel Roberts <yenevielroberts@gmail.com>
2025-10-25 03:43:45 +0200

    Enviar correo

---
+++ resources/views/resetPassword.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email-reset password</title>
+</head>
+<body>
+    <h1>¿Has olvidado tu contraseña?</h1>
+    <p>has click en el siguiente enlace para cambiar</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\PedidosController;
 use App\Http\Controllers\ProductosController;
 use App\Http\Controllers\UserController;
+use App\Mail\resetPassword;
+use Illuminate\Support\Facades\Mail;
 use App\Models\Pedido;
 use Illuminate\Support\Facades\Route;
 
@@ -10,12 +12,23 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcomPage');
 
+Route::post('/logout',[UserController::class,'logout'])->name('logout');
+
+//Protejo estas ruta de los usuarios ya logeados.
+//si ya esta logeado se le enviará a la welcom page
+Route::middleware('guest')->controller(UserController::class)->group(function (){
+
+    Route::get('/login','loginVista')->name('show.login');
+    Route::get('/signup','vistaRegistro')->name('registroVista');
+    Route::post('/login','login')->name('login');
+    Route::post('/signup','registro')->name('registro');
+});
 
 
-Route::get('/login',[UserController::class,'loginVista'])->name('show.login');
+//Cojo todas las rutas que quiero proteger y las agrupos aquí para que todas tenga el mismo middleware
+Route::middleware('auth')->group(function (){
 
-Route::get('/signup',[UserController::class,'vistaRegistro'])->name('registroVista');
-//Obtengo todas las categorias
+    //Obtengo todas las categorias
 Route::get('/categorias/get/',[ProductosController::class,'getCategorias'])->name('getCategorias');
 
 //Obtengo todos los prodcutos
@@ -26,11 +39,6 @@ Route::get('/products/get',[ProductosController::class,'getProductsByCategoria']
 
 Route::get('/pedido/create',[PedidosController::class,'formPedido'])->name('pedidos.form');
 
-Route::post('/login',[UserController::class,'login'])->name('login');
-Route::post('/signup',[UserController::class,'registro'])->name('registro');
-
-Route::post('/logout',[UserController::class,'logout'])->name('logout');
-
 //Busqueda
 Route::post('/search',[ProductosController::class,'search'])->name('search');
 
@@ -38,6 +46,19 @@ Route::post('/search',[ProductosController::class,'search'])->name('search');
 Route::post('/setOrder',[PedidosController::class,'setOrder'])->name('pedidos.setOrder');
 
 Route::get('/history',[PedidosController::class,'history'])->name('pedidos.history');
+});
+
+Route::get('/forgot', function(){
+
+     try {
+        Mail::to('mendezyeneviel@gmail.com')->send(new \App\Mail\resetPassword());
+        return "Mensaje enviado";
+    } catch (\Exception $e) {
+        return "Error al enviar el correo: " . $e->getMessage();
+    }
+});
+
+
 
 
 
